475f703b98f2636655156029ddc66e03988119cc
Zhang Shengce <shengcezhang@163.com>
2026-03-09 16:26:36 +0800

    copy necessary files from old sentry

--- DecisionNode/src/decision_node/include/decision_node/recover_change.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <behaviortree_cpp_v3/bt_factory.h>
-#include <ros/ros.h>
-
-void RegisterRecoverChangeNodes(BT::BehaviorTreeFactory& factory, ros::Publisher* recover_pub, ros::Publisher* bullet_up_pub);
-
-void RegisterBulletSupplyNodes(BT::BehaviorTreeFactory& factory, ros::Publisher* bullet_num_pub);
